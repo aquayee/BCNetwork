111baf9c0d8d1f52ab1926ee169a266185011e59
林柏参 <sshare@qq.com>
2014-11-14 11:48:09 +0800

    Test
    更新了缓存工具类 ,选择适合自己的枚举

--- GETPOST/GETPOST/BaseHttpTool.h
+++ GETPOST/GETPOST/BaseHttpTool.h
@@ -1,8 +1,7 @@
 /**
- *  这是一个简单的请求工具类,封装的网络请求 支持ASI 和 最新的AFN
+ *  封装的网络请求 支持ASI 和 最新的AFN
  *  默认使用 AFN 有其他需求需要使用ASI的，注视相关代码，打开ASI即可
  *  邮箱 : sshare@qq.com
- *  https://github.com/BaiCanLin/GETPOST
  */
 
 #import <Foundation/Foundation.h>
@@ -11,6 +10,24 @@ typedef void (^BaseHttpToolSucess)(id json);
 typedef void (^BaseHttpToolFailur)(NSError *error);
 
 @interface BaseHttpTool : NSObject
+
+typedef NS_ENUM(NSUInteger, BcRequestCenterCachePolicy) {
+    
+    /**
+     *  普通网络请求,不会有缓存
+     */
+    BcRequestCenterCachePolicyNormal,
+    
+    /**
+     *  如果有网络直接读网络，如果没网络直接读本地
+     */
+    BcRequestCenterCachePolicyCacheAndRefresh,
+    
+    /**
+     *  优先读取本地，不管有没有网络，优先读取本地
+     */
+    BcRequestCenterCachePolicyCacheAndLocal
+};
 
 /**
  *  普通的 post 请求
@@ -23,23 +40,14 @@ typedef void (^BaseHttpToolFailur)(NSError *error);
 +(void)postWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur;
 
 /**
- *  普通的 get 请求
- *
- *  @param url        接口地址 Url
- *  @param parameters 请求参数
- *  @param sucess     成功后的回调
- *  @param failur     失败后的回调
- */
-+(void)getWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur;
-
-/**
  *  带缓存的 get 请求
  *
  *  @param url        接口地址 Url
+ *  @param option     枚举,选择缓存策略
  *  @param parameters 请求参数
  *  @param sucess     成功后的回调
  *  @param failur     失败后的回调
  */
-+(void)getCacheWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur;
++(void)getCacheWithUrl:(NSString *)url option:(BcRequestCenterCachePolicy)option parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur;
 
 @end
--- GETPOST/GETPOST/BaseHttpTool.m
+++ GETPOST/GETPOST/BaseHttpTool.m
@@ -1,23 +1,19 @@
-/**
- *  封装的网络请求 支持ASI 和 最新的AFN
- *  默认使用 AFN 有其他需求需要使用ASI的，注视相关代码，打开ASI即可
- *  邮箱 : sshare@qq.com
- *  https://github.com/BaiCanLin/GETPOST
- */
 
 #import "BaseHttpTool.h"
 #import "Reachability.h"
 //#import "ASIFormDataRequest.h"
 #import "AFNetworking.h"
-#import "YTKKeyValueStore.h"
 #import "AFNetworkActivityIndicatorManager.h"
+#import "RNCachingURLProtocol.h"
 
 @implementation BaseHttpTool
 
-static YTKKeyValueStore *store;
+static YTKKeyValueStore *_store;
 
 +(void)postWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur
 {
+    [NSURLProtocol unregisterClass:[RNCachingURLProtocol class]];
+    
     // 1.创建POST请求
     AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
     
@@ -33,76 +29,130 @@ static YTKKeyValueStore *store;
           }
       }];
     
+    /**
+     *  这里是特殊需求 不需要webView 缓存直接注视掉
+     */
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [NSURLProtocol registerClass:[RNCachingURLProtocol class]];
+    });
+    
     // 这个底层封装的是ASI post请求
 //    [self requestWithMethod:@"POST" url:url parameters:parameters sucess:sucess failur:failur];
 }
 
-+(void)getWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur
++(void)getCacheWithUrl:(NSString *)url option:(BcRequestCenterCachePolicy)option parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur
 {
+    [NSURLProtocol unregisterClass:[RNCachingURLProtocol class]];
+    
     // 1.创建GET请求
     AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
     // 开启网络指示器
     [[AFNetworkActivityIndicatorManager sharedManager]setEnabled:YES];
-    
-    // 2.发送请求
-    [mgr GET:url parameters:parameters
-     success:^(AFHTTPRequestOperation *operation, id responseObject) {
-         
-         if (sucess) {
-             sucess(responseObject);
-         }
-     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-         if (failur) {
-             failur(error);
-         }
-     }];
-    
-    // 这个底层封装的是ASI get请求
-//    [BaseHttpTool requestWithMethod:@"GET" url:url parameters:parameters sucess:sucess failur:failur];
-}
-
-+(void)getCacheWithUrl:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur
-{
-    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
-    // 开启网络指示器
-    [[AFNetworkActivityIndicatorManager sharedManager]setEnabled:YES];
-    
-    // 判断网络连接
-    int status = [BaseHttpTool reachabilityConnectionNetWork];
+    [AFHTTPRequestSerializer serializer].timeoutInterval = 1;
     
     // 数据库
     NSString *tableName = @"user_table";
+    
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        store = [[YTKKeyValueStore alloc] initDBWithName:@"JsonCache.db"];
+        _store = [[YTKKeyValueStore alloc] initDBWithName:@"JsonCache.db"];
     });
-    [store createTableWithName:tableName];
-
-    if (status == 0) { // 没有网络
-        NSDictionary *queryUser = [store getObjectById:url fromTable:tableName];
-        if (queryUser) {
-            sucess(queryUser);
+    [_store createTableWithName:tableName];
+    
+    // 判断网络连接
+    int status = [self reachabilityConnectionNetWork];
+    
+    switch (option) {
+        case BcRequestCenterCachePolicyNormal:{ // 普通的网络请求
+            
+            [mgr GET:url parameters:parameters
+             success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                 if (sucess) {
+                     sucess(responseObject);
+                 }
+             } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                 if (failur) {
+                     failur(error);
+                 }
+             }];
         }
-        failur(failur);
-    }else if (status == 1) // 有网络
-    {
-        // 发送请求
-        [mgr GET:url parameters:parameters
-         success:^(AFHTTPRequestOperation *operation, id responseObject) {
-             
-             if (sucess) {
-                 [store putObject:responseObject withId:url intoTable:tableName];
-                 sucess(responseObject);
-             }
-         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-             if (failur) {
-                 failur(error);
-             }
-         }];
+            break;
+        case BcRequestCenterCachePolicyCacheAndRefresh:{ //如果有网络直接读网络，如果没网络直接读本地
+            
+            if (status == 0) { // 没有网络
+                NSDictionary *queryUser = [_store getObjectById:url fromTable:tableName];
+                if (queryUser) {
+                    sucess(queryUser);
+                }
+                failur(failur);
+            }else// 有网络
+            {
+                // 发送请求
+                [mgr GET:url parameters:parameters
+                 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                     [_store putObject:responseObject withId:url intoTable:tableName];
+                     if (sucess) {
+                         sucess(responseObject);
+                     }
+                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                     if (failur) {
+                         failur(error);
+                     }
+                 }];
+            }
+        }
+            break;
+        case BcRequestCenterCachePolicyCacheAndLocal:{ //优先读取本地，不管有没有网络，优先读取本地
+            
+            if (status == 0) { // 没有网络
+                id previouslySaved = [_store getObjectById:url fromTable:tableName];
+                if (previouslySaved) {
+                    sucess(previouslySaved);
+                }
+            }else// 有网络
+            {
+                // 发送请求
+                [mgr GET:url parameters:parameters
+                 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+
+                     if ([_store getObjectById:url fromTable:tableName]) {
+                         sucess([_store getObjectById:url fromTable:tableName]);
+                     }else
+                     {
+                         sucess(responseObject);
+                     }
+                     
+                     [_store putObject:responseObject withId:url intoTable:tableName];
+                     
+                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                     if (failur) {
+                         failur(error);
+                     }
+                 }];
+            }
+        }
+            break;
+        default:
+            break;
     }
+    
+    /**
+     *  这里是特殊需求 不需要webView 缓存直接注视掉
+     */
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [NSURLProtocol registerClass:[RNCachingURLProtocol class]];
+    });
+    
+    // 这个底层封装的是ASI get请求
+    //    [BaseHttpTool requestWithMethod:@"GET" url:url parameters:parameters sucess:sucess failur:failur];
 }
 
-#warning 如果需要使用 ASI 请注释 AFN 打开这里的 ASI
++(int)reachabilityConnectionNetWork
+{
+    Reachability *connectionNetWork = [Reachability reachabilityWithHostName:@"www.baidu.com"];
+    int status = [connectionNetWork currentReachabilityStatus];
+    return status;
+}
 
 //+(void)requestWithMethod:(NSString *)method url:(NSString *)url parameters:(NSDictionary *)parameters sucess:(BaseHttpToolSucess)sucess failur:(BaseHttpToolFailur)failur
 //{
@@ -112,7 +162,7 @@ static YTKKeyValueStore *store;
 //    //设置超时
 //    [request setTimeOutSeconds:1];
 //    [request setRequestMethod:method];
-//    
+//
 //    //设置表单提交项
 //    for(id key in parameters)
 //    {
@@ -123,13 +173,14 @@ static YTKKeyValueStore *store;
 //    //请求执行完会调用block中的代码
 ////    __unsafe_unretained ASIFormDataRequest *request1 = request;
 //    __weak typeof(request) request1 = request;
-//    
+//
 //    [request setCompletionBlock:^{
-//        
+//
 //        NSData *data = [request1 responseData];
 //        NSDictionary *datassin = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
 //        if (sucess) {
 //            sucess(datassin);
+////            NSLog(@"sucess=====%@",codes);
 //        }
 //    }];
 //
@@ -144,13 +195,17 @@ static YTKKeyValueStore *store;
 //}
 
 /**
- *  判断网络连接
+ *  后端需要 每个项目情况不同 根据不同需求 更改
  */
-+(int)reachabilityConnectionNetWork
-{
-    Reachability *connectionNetWork = [Reachability reachabilityWithHostName:@"www.baidu.com"];
-    int status = [connectionNetWork currentReachabilityStatus];
-    return status;
-}
+//    NSString *codes;
+//    NSUserDefaults *us=[NSUserDefaults standardUserDefaults];
+//    if ([us objectForKey:@"httpcode"]) {
+//        codes=[us objectForKey:@"httpcode"];
+//    }else{
+//        codes=[NSString stringWithFormat:@"\\$\\@-%d%d-SEXUALIFEHAIWANG",arc4random_uniform(100000),arc4random_uniform(100000)];
+//        [us setValue:codes forKey:@"httpcode"];
+//        [us synchronize];
+//    }
+//    [request setPostValue:codes forKey:@"CODE"];
 
 @end
