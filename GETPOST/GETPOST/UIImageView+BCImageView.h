ecd34910a1882ec131490d8007735fbeeaeb3982
A <A@AdeMacBook-Pro.lan>
2015-08-05 10:46:40 +0800

    更新缓存库，和一个简单的图片缓存，配合使用很简单

---
+++ GETPOST/GETPOST/UIImageView+BCImageView.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+BCImageView.h
+//  GETPOST
+//
+//  Created by A on 15/8/5.
+//  Copyright (c) 2015年 林柏参. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (BCImageView)
+
+@end
---
+++ GETPOST/GETPOST/UIImageView+BCImageView.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+BCImageView.m
+//  GETPOST
+//
+//  Created by A on 15/8/5.
+//  Copyright (c) 2015年 林柏参. All rights reserved.
+//
+
+#import "UIImageView+BCImageView.h"
+
+@implementation UIImageView (BCImageView)
+
+@end
